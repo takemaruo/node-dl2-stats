658b23bc867d7222fed409074e386cd6c379a5d5
takemaruo <comonon.life@gmail.com>
2016-05-30 19:38:53 +0900

    cheerio-httpcliでスクレイピングしたいけど、認証がうまくいかない。とりあえずfetchしてみる。"

--- dl2-stats.js
+++ dl2-stats.js
@@ -3,4 +3,8 @@
 * dl2-stats.js
 */
 
-console.log("hello!node.js")
+var client = require('cheerio-httpcli');
+
+client.fetch('https://card.dartslive.com/t/login.jsp', function (err, $, res, body) {
+        console.log(body)
+});
